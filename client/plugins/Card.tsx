9be2f167abdefa5507d4f814a037163ec2f9a3a4
Justin <jchu231@gmail.com>
2021-10-08 13:04:47 -0700

    adding card component

--- client/plugins/Card.tsx
+++ client/plugins/Card.tsx
@@ -5,7 +5,8 @@ import reactToWebComponent from "react-to-webcomponent";
 import { PlayerState, Card } from "../.rtag/types";
 
 function CardComponent({ val, state }: { val: Card; state: PlayerState }) {
-  return <div style={{ width: 50, height: 100, textAlign: "center" }}>{val.details?.name}</div>;
+  return val.details ? (<img style={{ width: 100, height: 150 }} src={`/${val.details?.name.toLowerCase()}.png`} alt=""/>)
+      : (<img style={{ width: 100, height: 150 }} src={`/card_back.png`} alt=""/>);
 }
 
 export default reactToWebComponent(CardComponent, React, ReactDom);
